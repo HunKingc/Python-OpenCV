1cae087599f674153e0ebebe638af83a9d66b06b
HunKingc <102198527+HunKingc@users.noreply.github.com>
2022-12-24 11:03:52 +0800

    Delete Bezel.py

--- Bezel.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-import numpy as np
-# 显示一个blank空白窗口
-img=cv.imread("E:/OpenCV/data/cat.jpg")
-black=np.zeros(img.shape,dtype='uint8')
-cv.imshow('Blank',black)
-cv.waitKey()
-cv.destroyAllWindows()

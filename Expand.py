15b4abe953597768a2dacdd7bca2a730f3fef6f6
HunKingc <102198527+HunKingc@users.noreply.github.com>
2022-12-24 11:04:35 +0800

    Delete Expand.py

--- Expand.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-import numpy as np
-# 膨胀
-img=cv.imread("E:/OpenCV/data/cat.jpg")
-canny=cv.Canny(img,125,175)
-dilated=cv.dilate(canny,(7,7),iterations=3)
-cv.imshow('Dilated',dilated)
-cv.waitKey(0)

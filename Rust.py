f50221948b5edc711544e2a959c0a81632bc1a5c
HunKingc <102198527+HunKingc@users.noreply.github.com>
2022-12-24 11:05:01 +0800

    Delete Rust.py

--- Rust.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-# 腐蚀
-img=cv.imread("E:/OpenCV/data/cat.jpg")
-canny=cv.Canny(img,125,175)
-dilated=cv.dilate(canny,(7,7),iterations=3)
-eroded=cv.erode(dilated,(7,7),iterations=3)
-cv.imshow('Eroded',eroded)
-cv.waitKey(0)
